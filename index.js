f86041d95050118b27d7bbf4cdb12a3c3b09b675
gonzalezm6 <31254265+gonzalezm6@users.noreply.github.com>
2018-05-01 00:17:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var socket = require('socket.io');
 
 //App setup
 var app = express();
-server.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
    var host = server.address().address;
    var port = server.address().port;
    console.log('Listening on http://%s:%s', host, port);
